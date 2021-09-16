52d33d209c4988cefd041f5c4178295eb8c3fb1b
M-Stable <fchanghlee@gmail.com>
2021-09-16 14:11:16 +1200

    basic set up of backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.set;
+
+server.listen(3001);
