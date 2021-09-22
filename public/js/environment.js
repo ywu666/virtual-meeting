642c44dc2f3993952692338ab86ba6a7d98ab9bc
ywu666 <ywu660@aucklanduni.ac.nz>
2021-09-22 15:15:15 +0800

    Add actions of avatars as a gui on the screen.

--- public/js/environment.js
+++ public/js/environment.js
@@ -1,7 +1,8 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import { GUI } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/libs/dat.gui.module.js';
 
-let myMesh;
+let myMesh, gui, actions, previousAction, activeAction;
 const loader = new GLTFLoader();
 let mixer;
 var clock = new THREE.Clock();
@@ -87,12 +88,15 @@ function loadAvatar(playerGroup) {
     "./resources/robot_expressive/scene.gltf",
     (gltf) => {
       chars = gltf.scene;
-
-      const animations = gltf.animations;
-      console.log(animations);
+      const avatarnAimations = gltf.animations;
+      console.log(avatarnAimations);
       mixer = new THREE.AnimationMixer(chars);
-      const action = mixer.clipAction(animations[10]);
-      action.reset().play();
+      activeAction = mixer.clipAction(avatarnAimations[10]);
+      activeAction.play();
+
+      if(typeof gui == "undefined") {
+          createAnimationsGUI(avatarnAimations, mixer);
+      }
 
       chars.scale.set(0.3, 0.3, 0.3);
       chars.position.set(0, -2, 0);
@@ -106,8 +110,75 @@ function loadAvatar(playerGroup) {
   );
 }
 
+// https://github.com/mrdoob/three.js/blob/master/examples/webgl_animation_skinning_morph.html
+function createAnimationsGUI(avatarnAimations, mixer) {
+    const emotes = ['Walking','Jump', 'Yes', 'No', 'Wave', 'ThumbsUp' ];
+    gui = new GUI();
+    actions = {};
+
+    for ( let i = 0; i < avatarnAimations.length; i ++ ) {
+        const clip = avatarnAimations[ i ];
+        const action = mixer.clipAction( clip );
+        actions[ clip.name ] = action;
+
+        if ( emotes.indexOf( clip.name ) >= 0) {
+            action.clampWhenFinished = true;
+            action.loop = THREE.LoopOnce;
+        }
+    }
+
+    console.log(actions);
+
+    const api = { state: 'Walking' };
+    // emotes
+    const emoteFolder = gui.addFolder( 'Actions' );
+    function createEmoteCallback( name ) {
+        api[ name ] = function () {
+            console.log('call call back' + name);
+            fadeToAction( name, 0.2 );
+            mixer.addEventListener( 'finished', restoreState);
+        };
+        emoteFolder.add( api, name );
+    }
+
+    console.log(emoteFolder);
+
+    function restoreState() {
+        console.log("call restoreState");
+        mixer.removeEventListener( 'finished', restoreState );
+        console.log(api.state);
+        fadeToAction( api.state, 0.2 );
+    }
+
+    for ( let i = 0; i < emotes.length; i ++ ) {
+        createEmoteCallback( emotes[ i ] );
+    }
+    emoteFolder.open();
+
+    console.log(api);
+
+    function fadeToAction( name, duration ) {
+        previousAction = activeAction;
+        activeAction = actions[ name ];
+        if ( previousAction !== activeAction ) {
+            previousAction.fadeOut( duration );
+        }
+        console.log("previous: ",previousAction);
+        console.log('active: ',activeAction);
+
+        activeAction.reset()
+            .setEffectiveTimeScale(1)
+            .setEffectiveWeight(1)
+            .fadeIn(duration)
+            .play();
+
+    }
+}
+
+
 function avatarWalk() {
   const delta = clock.getDelta();
+  console.log(delta);
   if (typeof mixer !== "undefined") {
     mixer.update(delta);
   }
@@ -120,3 +191,4 @@ function updateEnvironment(scene) {
 window.createEnvironment = createEnvironment;
 window.loadAvatar = loadAvatar;
 window.avatarWalk = avatarWalk;
+
