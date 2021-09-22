c43a50ba8d7052d6110759eaf69e8ce0c3b18208
justinkrojos <justinkrojos@gmail.com>
2021-09-22 20:35:33 +1200

    Fixed walking animation

--- public/js/environment.js
+++ public/js/environment.js
@@ -1,6 +1,6 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-import { GUI } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/libs/dat.gui.module.js';
+import { GUI } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/libs/dat.gui.module.js";
 
 let myMesh, gui, actions, previousAction, activeAction;
 const loader = new GLTFLoader();
@@ -82,20 +82,21 @@ function createEnvironment(scene) {
   );
 }
 
+let avatarAnimations;
+
 function loadAvatar(playerGroup) {
   let chars;
   loader.load(
     "./resources/robot_expressive/scene.gltf",
     (gltf) => {
       chars = gltf.scene;
-      const avatarnAimations = gltf.animations;
-      console.log(avatarnAimations);
+      avatarAnimations = gltf.animations;
       mixer = new THREE.AnimationMixer(chars);
-      activeAction = mixer.clipAction(avatarnAimations[10]);
+      activeAction = mixer.clipAction(avatarAnimations[3]);
       activeAction.play();
 
-      if(typeof gui == "undefined") {
-          createAnimationsGUI(avatarnAimations, mixer);
+      if (typeof gui == "undefined") {
+        createAnimationsGUI(avatarAnimations, mixer);
       }
 
       chars.scale.set(0.3, 0.3, 0.3);
@@ -111,74 +112,75 @@ function loadAvatar(playerGroup) {
 }
 
 // https://github.com/mrdoob/three.js/blob/master/examples/webgl_animation_skinning_morph.html
-function createAnimationsGUI(avatarnAimations, mixer) {
-    const emotes = ['Walking','Jump', 'Yes', 'No', 'Wave', 'ThumbsUp' ];
-    gui = new GUI();
-    actions = {};
+function createAnimationsGUI(avatarAnimations, mixer) {
+  const emotes = ["Idle", "Jump", "Yes", "No", "Wave", "ThumbsUp"];
+  gui = new GUI();
+  actions = {};
 
-    for ( let i = 0; i < avatarnAimations.length; i ++ ) {
-        const clip = avatarnAimations[ i ];
-        const action = mixer.clipAction( clip );
-        actions[ clip.name ] = action;
+  for (let i = 0; i < avatarAnimations.length; i++) {
+    const clip = avatarAnimations[i];
+    const action = mixer.clipAction(clip);
+    actions[clip.name] = action;
 
-        if ( emotes.indexOf( clip.name ) >= 0) {
-            action.clampWhenFinished = true;
-            action.loop = THREE.LoopOnce;
-        }
+    if (emotes.indexOf(clip.name) >= 0) {
+      action.clampWhenFinished = true;
+      action.loop = THREE.LoopOnce;
+    }
+  }
+
+  const api = { state: "Idle" };
+  // emotes
+  const emoteFolder = gui.addFolder("Actions");
+  function createEmoteCallback(name) {
+    api[name] = function () {
+      fadeToAction(name, 0.2);
+      mixer.addEventListener("finished", restoreState);
+    };
+    emoteFolder.add(api, name);
+  }
+
+  function restoreState() {
+    mixer.removeEventListener("finished", restoreState);
+    fadeToAction(api.state, 0.2);
+  }
+
+  for (let i = 0; i < emotes.length; i++) {
+    createEmoteCallback(emotes[i]);
+  }
+  emoteFolder.open();
+
+  function fadeToAction(name, duration) {
+    previousAction = activeAction;
+    activeAction = actions[name];
+    if (previousAction !== activeAction) {
+      previousAction.fadeOut(duration);
     }
 
-    console.log(actions);
-
-    const api = { state: 'Walking' };
-    // emotes
-    const emoteFolder = gui.addFolder( 'Actions' );
-    function createEmoteCallback( name ) {
-        api[ name ] = function () {
-            console.log('call call back' + name);
-            fadeToAction( name, 0.2 );
-            mixer.addEventListener( 'finished', restoreState);
-        };
-        emoteFolder.add( api, name );
-    }
-
-    console.log(emoteFolder);
-
-    function restoreState() {
-        console.log("call restoreState");
-        mixer.removeEventListener( 'finished', restoreState );
-        console.log(api.state);
-        fadeToAction( api.state, 0.2 );
-    }
-
-    for ( let i = 0; i < emotes.length; i ++ ) {
-        createEmoteCallback( emotes[ i ] );
-    }
-    emoteFolder.open();
-
-    console.log(api);
-
-    function fadeToAction( name, duration ) {
-        previousAction = activeAction;
-        activeAction = actions[ name ];
-        if ( previousAction !== activeAction ) {
-            previousAction.fadeOut( duration );
-        }
-        console.log("previous: ",previousAction);
-        console.log('active: ',activeAction);
-
-        activeAction.reset()
-            .setEffectiveTimeScale(1)
-            .setEffectiveWeight(1)
-            .fadeIn(duration)
-            .play();
-
-    }
+    activeAction
+      .reset()
+      .setEffectiveTimeScale(1)
+      .setEffectiveWeight(1)
+      .fadeIn(duration)
+      .play();
+  }
 }
 
+function animateWalk() {
+  activeAction = mixer.clipAction(avatarAnimations[10]);
+  activeAction.play();
+}
 
-function avatarWalk() {
+function animateIdle() {
+  if (
+    typeof activeAction !== "undefined" &&
+    activeAction.getClip().name === "Walking"
+  ) {
+    activeAction.stop();
+  }
+}
+
+function animate() {
   const delta = clock.getDelta();
-  console.log(delta);
   if (typeof mixer !== "undefined") {
     mixer.update(delta);
   }
@@ -190,5 +192,6 @@ function updateEnvironment(scene) {
 
 window.createEnvironment = createEnvironment;
 window.loadAvatar = loadAvatar;
-window.avatarWalk = avatarWalk;
-
+window.animateWalk = animateWalk;
+window.animateIdle = animateIdle;
+window.animate = animate;
